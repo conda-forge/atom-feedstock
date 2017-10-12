55eaa2e9d28ced0d56ee68384e786dd817809a42
Matthieu Dartiailh <marul@laposte.net>
2017-10-12 21:40:05 +0200

    Properly set CFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export CFLAGS="-D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS"
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
 # Add more build steps here, if they are necessary.
