4c16790ff72d2454c767745fa89d0ef6b3b5e6ea
MatthieuDartiailh <marul@laposte.net>
2018-08-08 22:18:07 -0400

    recipe: attempt to update to conda build 3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Fix an issue with MAX_SIZE definition 
+# Fix an issue with MAX_SIZE definition
 # ref: https://stackoverflow.com/a/30472875
 export CFLAGS="-D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS"
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 
 # Add more build steps here, if they are necessary.
 
